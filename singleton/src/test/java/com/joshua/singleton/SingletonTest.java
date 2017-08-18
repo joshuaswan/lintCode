259167912ca73e91fcb3e536caa496339222e5b5
joshuaswan <joshuaswan@sina.com>
2017-08-18 15:57:17 +0800

    [joshua]create singleton

---
+++ singleton/src/test/java/com/joshua/singleton/SingletonTest.java
@@ -0,0 +1,21 @@
+package com.joshua.singleton;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by joshua on 2017/8/18.
+ */
+public class SingletonTest {
+
+    @Test
+    public void test() {
+        Singleton A = Singleton.getInstance();
+        Singleton B = Singleton.getInstance();
+
+        assertThat(A == B, is(true));
+    }
+
+}
