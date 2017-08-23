ffe6c0abe73e31cdf1257dde2b4b4e79d0c6d381
joshuaswan <joshuaswan@sina.com>
2017-08-23 17:52:46 +0800

    [joshua]在网上找到对应递归方式遍历对应数字，从M中选择N个不同的数字。但是三角形数量问题还没有解决。

---
+++ triangleCount/src/main/java/com/joshua/triangleCount/Combination.java
@@ -0,0 +1,81 @@
+package com.joshua.triangleCount;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+/**
+ * Created by joshua on 2017/8/23.
+ */
+public class Combination {
+
+    private static int NUM = 0;
+
+    public void combine(List<String> datas){
+        TreeSet<String> set = new TreeSet<String>();
+        for (String string: datas){
+            set.add(string);
+        }
+
+        ArrayList<TreeSet<String>> subSet = getSubSet(set);
+        for (TreeSet<String> treeSet:subSet){
+            System.out.println(treeSet);
+        }
+    }
+
+    public String[] getBinaryValue(TreeSet<String> set){
+        int size = set.size();
+        int m = (int) Math.pow(2,size) -1;
+        String[] result = new String[m+1];
+        for (int i=m;i>-1;i--){
+            StringBuffer stringBuffer = new StringBuffer(Integer.toBinaryString(i));
+            int length = stringBuffer.length();
+            if (length < size){
+                for (int j=0;j<size -length; j++){
+                    stringBuffer.insert(0,"0");
+                }
+            }
+            result[i] = stringBuffer.toString();
+        }
+        return result;
+    }
+
+    public ArrayList<TreeSet<String>> getSubSet(TreeSet<String> set) {
+        ArrayList<TreeSet<String>> result = new ArrayList<TreeSet<String>>();
+        String[] items = new String[set.size()];
+        int i=0 ;
+        for (String item: set){
+            items[i++] =item;
+        }
+        String[] binaryValue = getBinaryValue(set);
+        for (int j=0;j<binaryValue.length;j++){
+            String value = binaryValue[j];
+            TreeSet<String> subSet = new TreeSet<String>();
+            for (int k=0;k<value.length();k++){
+                if (value.charAt(k) == '1'){
+                    subSet.add(items[k]);
+                }
+            }
+            if (subSet.size() == NUM){
+                result.add(subSet);
+            }
+        }
+        return result;
+    }
+
+    public void recursionCombine(int[] source,int start,int[] result,int index,int n){
+        int count = 0;
+        for (count = start;count < source.length - index + 1;count ++){
+            result[index - 1] = count;
+            if (index - 1 == 0){
+                for (int j= n-1;j>=0;j--){
+                    System.out.print(source[result[j]]);
+                }
+                System.out.println();
+            }else {
+                recursionCombine(source,count+1,result,index-1,n);
+            }
+        }
+    }
+}
+
---
+++ triangleCount/src/main/java/com/joshua/triangleCount/TriangleCount.java
@@ -0,0 +1,52 @@
+package com.joshua.triangleCount;
+
+/**
+ * Created by joshua on 2017/8/23.
+ */
+public class TriangleCount {
+    /**
+     * @param S: A list of integers
+     * @return: An integer
+     */
+    public int triangleCount(int[] S) {
+        // write your code here
+        if (S.length < 3){
+            return 0;
+        }
+        int triangleNumber = combination(S.length, 3);
+        int[][] results = new int[3][triangleNumber];
+
+        int count = 0;
+        for (int i=0;i<triangleNumber;i++){
+            if (isTriangle(results[i])){
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    public int factorial(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+
+    public int combination(int m, int n) {
+        if (m < n) {
+            return 0;
+        }
+        return factorial(m) / factorial(n) / factorial(m - n);
+    }
+
+    public boolean isTriangle(int[] edge) {
+        if (edge.length != 3) {
+            return false;
+        }
+        return (edge[0] + edge[1] > edge[2] &&
+                edge[0] + edge[2] > edge[1] &&
+                edge[1] + edge[2] > edge[0]) ? true : false;
+    }
+
+}
