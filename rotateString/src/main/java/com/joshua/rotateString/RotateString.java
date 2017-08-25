d744e12b1ca5dd491e22256cfcfdcf89b5b71c71
joshuaswan <joshuaswan@sina.com>
2017-08-25 16:43:03 +0800

    [joshua]It's become to difficult to complete the question. Maybe learn some book is a good idea.

---
+++ rotateString/src/main/java/com/joshua/rotateString/RotateString.java
@@ -0,0 +1,16 @@
+package com.joshua.rotateString;
+
+/**
+ * Created by joshua on 2017/8/25.
+ */
+public class RotateString {
+
+    /**
+     * @param str: an array of char
+     * @param offset: an integer
+     * @return: nothing
+     */
+    public void rotateString(char[] str, int offset) {
+        // write your code here
+    }
+}
